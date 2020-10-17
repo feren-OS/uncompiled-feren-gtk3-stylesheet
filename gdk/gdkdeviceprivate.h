tschoonj/gtk

GdkDeviceTool: Add GdkDeviceToolType to identify the physical tool type
Because there are multiple different types of styluses that can be used with
tablets, we have to have some sort of identifier for them attached to the
GdkDeviceTool, especially since knowing the actual tool type for a GdkDeviceTool
is necessary for matching up a GdkDeviceTool with it's appropriate
GdkInputSource in Wayland (eg. matching up a GdkDeviceTool eraser with the
GDK_SOURCE_ERASER GdkInputSource of a wayland tablet).

Signed-off-by: Stephen Chandler Paul <thatslyude@gmail.com>

gdk/gdkdeviceprivate.h
@@ -34,6 +34,7 @@ typedef struct _GdkDeviceKey GdkDeviceKey;
 struct _GdkDeviceTool
 {
   guint serial;
+  GdkDeviceToolType type;
   gint ref_count;
 };
 
@@ -187,13 +188,14 @@ GdkWindow * _gdk_device_window_at_position    (GdkDevice        *device,
                                                gboolean          get_toplevel);
 
 /* Device tools */
-GdkDeviceTool *gdk_device_tool_new    (guint          serial);
-GdkDeviceTool *gdk_device_lookup_tool (GdkDevice     *device,
-                                       guint          serial);
-void           gdk_device_add_tool    (GdkDevice     *device,
-                                       GdkDeviceTool *tool);
-void           gdk_device_update_tool (GdkDevice     *device,
-                                       GdkDeviceTool *tool);
+GdkDeviceTool *gdk_device_tool_new    (guint              serial,
+                                       GdkDeviceToolType  type);
+GdkDeviceTool *gdk_device_lookup_tool (GdkDevice         *device,
+                                       guint              serial);
+void           gdk_device_add_tool    (GdkDevice         *device,
+                                       GdkDeviceTool     *tool);
+void           gdk_device_update_tool (GdkDevice         *device,
+                                       GdkDeviceTool     *tool);
 
 GdkDeviceTool *gdk_device_tool_ref    (GdkDeviceTool *tool);
 void           gdk_device_tool_unref  (GdkDeviceTool *tool);

feren-OS/uncompiled-feren-gtk3-stylesheet

gdk/gdkdeviceprivate.h
@@ -37,6 +37,7 @@ struct _GdkDeviceTool
 {
   GObject parent_instance;
   guint64 serial;
+  GdkDeviceToolType type;
 };
 
 struct _GdkDeviceToolClass
@@ -198,7 +199,8 @@ void  gdk_device_set_seat  (GdkDevice *device,
                             GdkSeat   *seat);
 
 /* Device tools */
-GdkDeviceTool *gdk_device_tool_new    (guint64        serial);
+GdkDeviceTool *gdk_device_tool_new    (guint64            serial,
+                                       GdkDeviceToolType  type);
 void           gdk_device_update_tool (GdkDevice     *device,
                                        GdkDeviceTool *tool);
 