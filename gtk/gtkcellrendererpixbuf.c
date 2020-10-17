GNOME/gtk

cellrendererpixbuf: Use fallback icons with icon-name
This gives us a better chance of finding an icon to show.

Closes: #1809

gtk/gtkcellrendererpixbuf.c
@@ -391,6 +391,7 @@ create_icon_helper (
 
   icon_helper = gtk_icon_helper_new (gtk_style_context_get_node (gtk_widget_get_style_context (widget)),
                                      widget);
+  _gtk_icon_helper_set_use_fallback (icon_helper, TRUE);
   _gtk_icon_helper_set_force_scale_pixbuf (icon_helper, TRUE);
   _gtk_icon_helper_set_definition (icon_helper, priv->image_def);
 

feren-OS/uncompiled-feren-gtk3-stylesheet

(cherry picked from commit fbe0e328137714de00cd1b7d47f77b539092ac9a)

gtk/gtkcellrendererpixbuf.c
@@ -464,6 +464,7 @@ create_icon_helper (GtkCellRendererPixbuf *cellpixbuf,
   GtkIconHelper *helper;
 
   helper = gtk_icon_helper_new (gtk_style_context_get_node (gtk_widget_get_style_context (widget)), widget);
+  _gtk_icon_helper_set_use_fallback (helper, TRUE);
   _gtk_icon_helper_set_force_scale_pixbuf (helper, TRUE);
   _gtk_icon_helper_set_definition (helper, priv->image_def);
   if (gtk_image_definition_get_storage_type (priv->image_def) != GTK_IMAGE_PIXBUF)