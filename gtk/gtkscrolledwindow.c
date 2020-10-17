hayate891/gtk

scrolledwindow: Avoid critical when removing source
Make sure indicator->conceil_timer is actually set before trying to
remove it.

gtk/gtkscrolledwindow.c
@@ -3821,7 +3821,8 @@ indicator_set_fade (Indicator *indicator,
     {
       indicator->conceil_timer = g_timeout_add (INDICATOR_FADE_OUT_TIME, maybe_hide_indicator, indicator);
     }
-  if (!visible && gtk_widget_get_mapped (indicator->scrollbar))
+  if (!visible && gtk_widget_get_mapped (indicator->scrollbar) &&
+      indicator->conceil_timer != 0)
     {
       g_source_remove (indicator->conceil_timer);
       indicator->conceil_timer = 0;

feren-OS/uncompiled-feren-gtk3-stylesheet

gtk/gtkscrolledwindow.c
@@ -4192,7 +4192,8 @@ indicator_set_fade (Indicator *indicator,
       gdk_window_show (indicator->window);
       indicator->conceil_timer = g_timeout_add (INDICATOR_FADE_OUT_TIME, maybe_hide_indicator, indicator);
     }
-  if (!visible && gdk_window_is_visible (indicator->window))
+  if (!visible && gdk_window_is_visible (indicator->window) &&
+      indicator->conceil_timer != 0)
     {
       gdk_window_hide (indicator->window);
       g_source_remove (indicator->conceil_timer);